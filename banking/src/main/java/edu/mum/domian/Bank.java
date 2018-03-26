cf81289bcd9d185228d76803ad5030297965be2d
yaried tekie <yaredsonj@gmail.com>
2018-03-26 12:21:21 -0500

    LaonApplication, Bank and withdrwal domain classes are created

---
+++ banking/src/main/java/edu/mum/domian/Bank.java
@@ -0,0 +1,20 @@
+package edu.mum.domian;
+
+import java.util.List;
+
+public class Bank {
+
+	private String Name;
+	private Long routing_Number;
+	
+	private List<Address> address;
+	
+	private List<Customer> customer;
+	
+	private List<RegulatoryAgencies> regulatoryAgencies;
+	
+	private List<Account> account;
+	
+	private List<Loan> loan;
+	
+}
