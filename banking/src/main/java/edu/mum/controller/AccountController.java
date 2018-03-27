90925b9d53cce86fac4d9968cdac02a6c3fc539a
Mulugeta-Bisrat <34776292+Mulugeta-Bisrat@users.noreply.github.com>
2018-03-27 08:47:41 -0700

    modified config

--- banking/src/main/java/edu/mum/controller/AccountController.java
+++ banking/src/main/java/edu/mum/controller/AccountController.java
@@ -1,13 +1,8 @@
 package edu.mum.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+
 public class AccountController {
 
-	@RequestMapping("/")
-	public String welcome() {
-		return "home";
-	}
+	
 }
